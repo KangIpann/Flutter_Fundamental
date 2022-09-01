c1c14dc0c9f0bcc53e2257aa77f5688d6bee91e5
Ipannn <ivakaopsionalacount@gmail.com>
2022-09-01 18:48:50 +0700

    Update Materi

---
+++ animated_container/lib/main.dart
@@ -0,0 +1,44 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Random random = Random();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Animated Container"),),
+        body: Center(
+          //Untuk mendeteksi tap
+          child: GestureDetector(
+            onTap: (){
+              setState(() {
+              });
+            },
+            child: AnimatedContainer(
+              color: Color.fromARGB(255, random.nextInt(256), random.nextInt(256), random.nextInt(256)),
+              duration: Duration(seconds: 1),
+              width: 50.0 + random.nextInt(101),
+              height: 50.0 + random.nextInt(101),
+            ),
+          )
+          ),
+      ),
+    );
+  }
+}
+
+
