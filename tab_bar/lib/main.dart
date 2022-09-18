16cf41772020a191fd2101fe857dd0c98bc8824d
Ipannn <ivakaopsionalacount@gmail.com>
2022-09-18 18:11:54 +0700

    Decoration Tab Bar

--- tab_bar/lib/main.dart
+++ tab_bar/lib/main.dart
@@ -9,29 +9,41 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TabBar myTabBar = TabBar(
+      // indicatorColor: Colors.red,
+      indicator: BoxDecoration(
+        color: Colors.purple,
+        border: Border(bottom: BorderSide(color: Colors.red, width: 5)),
+      ),
+      tabs: [
+        Tab(icon: Icon(Icons.comment), text: "Comments",),
+        // Tab(child: Container(
+        //   width: 30,
+        //   height: 50,
+        //   child: Column(
+        //     children: [
+        //       Image(image: NetworkImage("https://img.lovepik.com/free_png/28/78/47/79g58PICpbi80jfJN53ey_PIC2018.png_300.png"),),
+        //       Text("Cute")
+        //     ],
+        //   ),
+        // ),
+        // ),
+        Tab(icon: Icon(Icons.computer),text: "Computers",),
+        // Tab(text: "News",)
+      ],
+    );
     return MaterialApp(
       home: DefaultTabController(
-        length: 4,
+        length: 2,
         child: Scaffold(
           appBar: AppBar(title: Text("Contoh Tab Bar"),
-          bottom: TabBar(
-            tabs: [
-              Tab(icon: Icon(Icons.comment), text: "Comments",),
-              Tab(child: Container(
-                width: 30,
-                height: 50,
-                child: Column(
-                  children: [
-                    Image(image: NetworkImage("https://img.lovepik.com/free_png/28/78/47/79g58PICpbi80jfJN53ey_PIC2018.png_300.png"),),
-                    Text("Cute")
-                  ],
-                ),
-              ),
-              ),
-              Tab(icon: Icon(Icons.computer),),
-              Tab(text: "News",)
-            ],
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(myTabBar.preferredSize.height),
+            child: Container(
+              color: Colors.amber,
+                child: myTabBar,
             ),
+          ),
           ),
           body: TabBarView(
             children: [
@@ -41,12 +53,12 @@ class MyApp extends StatelessWidget {
               Center(
                 child: Text("Tab 2"),
               ),
-              Center(
-                child: Text("Tab 3"),
-              ),
-              Center(
-                child: Text("Tab 4"),
-              ),
+              // Center(
+              //   child: Text("Tab 3"),
+              // ),
+              // Center(
+              //   child: Text("Tab 4"),
+              // ),
             ],
           ),
         ),
